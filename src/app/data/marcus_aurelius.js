fe481a3be5b7a292d7dea519c64af1c36f325174
Canewt <ksangram01@gmail.com>
2024-08-25 19:39:41 +0530

    Add more Marcus Aurelius quotes to the collection

--- src/app/data/marcus_aurelius.js
+++ src/app/data/marcus_aurelius.js
@@ -1,7 +1,19 @@
 const quotes = [
-  "You have power over your mind - not outside events. Realize this, and you will find strength.",
-  "The happiness of your life depends upon the quality of your thoughts.",
-  "Waste no more time arguing about what a good man should be. Be one."
+  "Choose not to be harmed, and you won't feel harmed. Don't feel harmed, and you haven't been.",
+  "You have power over your mind — not outside events. Realize this, and you will find strength.",
+  "Waste no more time arguing about what a good man should be. Be one.",
+  "The things you think about determine the quality of your mind. Your soul takes on the color of your thoughts.",
+  "I have often wondered how it is that every man loves himself more than all the rest of men, but yet sets less value on his own opinion of himself than on the opinion of others.",
+  "Be tolerant with others and strict with yourself.",
+  "Begin each day by telling yourself: Today I will meet people who are meddling, ungrateful, insolent, disloyal, malicious and selfish—all because of their ignorance of what is good and evil.",
+  "The best revenge is not to be like your enemy.",
+  "Accept the things to which fate binds you, and love the people with whom fate brings you together, but do so with all your heart.",
+  "Whenever you are about to find fault with someone, ask yourself the following question: What fault of mine most nearly resembles the one I am about to criticize?",
+  "Very little is needed to make a happy life; it is all within yourself, in your way of thinking.",
+  "Dwell on the beauty of life. Watch the stars, and see yourself running with them.",
+  "Never let the future disturb you. You will meet it, if you have to, with the same weapons of reason which today arm you against the present.",
+  "Do not indulge in dreams of having what you have not, but reckon up the chief of the blessings you do possess, and then thankfully remember how you would crave for them if they were not yours.",
+  "Think of the life you have lived until now as over and, as a dead man, see what's left as a bonus and live it according to Nature. Love the hand that fate deals you and play it as your own, for what could be more fitting?"
 ];
 
 export default quotes;
